36bed4c6ec79b81e5ab613c4987a9d4d38105f80
IL KYOUNG HWANG <sizuha@gngs.co.jp>
2019-04-10 17:56:20 +0900

    + YearMonthDay

---
+++ SizUtil/SizUtil/SizUtil.swift
@@ -0,0 +1,81 @@
+//
+//  SizUtil.swift
+//  SizUtil
+//
+//  Created by IL KYOUNG HWANG on 2019/04/10.
+//  Copyright © 2019 Sizuha. All rights reserved.
+//
+
+import Foundation
+
+public struct YearMonthDay: Equatable {
+	
+	public let year: Int
+	public let month: Int
+	public let day: Int
+	
+	let calendar = Calendar(identifier: .gregorian)
+	
+	public init(_ year: Int, _ month: Int, _ day: Int) {
+		self.year = year
+		self.month = month
+		self.day = day
+	}
+	
+	public init(from date: Date) {
+		year = calendar.component(.year, from: date)
+		month = calendar.component(.month, from: date)
+		day = calendar.component(.day, from: date)
+	}
+	
+	public init (from comp: DateComponents) {
+		year = comp.year ?? 1
+		month = comp.month ?? 1
+		day = comp.day ?? 1
+	}
+	
+	public func toDate(timeZone: TimeZone = TimeZone.current) -> Date? {
+		let comp = toDateComponents(timeZone: timeZone)
+		return calendar.date(from: comp)
+	}
+	
+	public func toDateComponents(timeZone: TimeZone = TimeZone.current) -> DateComponents {
+		var comp = DateComponents()
+		comp.year = year
+		comp.month = month
+		comp.day = day
+		comp.hour = 0
+		comp.minute = 0
+		comp.second = 0
+		comp.timeZone = timeZone
+		return comp
+	}
+	
+	public func toInt() -> Int {
+		return year*100_00 + month*100 + day
+	}
+	
+	public static func == (lhs: YearMonthDay, rhs: YearMonthDay) -> Bool {
+		return lhs.toInt() == rhs.toInt()
+	}
+	
+	public func add(year: Int = 0, month: Int = 0, day: Int = 0) -> YearMonthDay? {
+		guard let date = toDate() else { return nil }
+		
+		var comp = DateComponents()
+		comp.year = year
+		comp.month = month
+		comp.day = day
+		
+		guard let added = calendar.date(byAdding: comp, to: date) else { return nil }
+		return YearMonthDay(from: added)
+	}
+	
+	public func days(from: YearMonthDay) -> Int? {
+		guard let fromDate = from.toDate() else { return nil }
+		guard let toDate = self.toDate() else { return nil }
+		
+		return calendar.dateComponents([.day], from: fromDate, to: toDate).day
+	}
+
+}
--- SizUtil/SizUtilTests/SizUtilTests.swift
+++ SizUtil/SizUtilTests/SizUtilTests.swift
@@ -19,16 +19,34 @@ class SizUtilTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testYearMonthDay() {
+		let src = YearMonthDay(2019, 4, 10)
+		if let date = src.toDate() {
+			print("YearMonthDay to Date:", date)
+			
+			let cal = Calendar(identifier: .gregorian)
+			XCTAssertEqual(cal.component(.year, from: date), 2019)
+			XCTAssertEqual(cal.component(.month, from: date), 4)
+			XCTAssertEqual(cal.component(.day, from: date), 10)
+			XCTAssertEqual(cal.component(.hour, from: date), 0)
+			XCTAssertEqual(cal.component(.minute, from: date), 0)
+			XCTAssertEqual(cal.component(.second, from: date), 0)
+		}
+		else {
+			XCTAssert(false)
+		}
+		
+		let src2 = YearMonthDay(2019, 4, 10)
+		XCTAssertEqual(src, src2)
+		
+		XCTAssertEqual(src2.add(month: -4) , YearMonthDay(2018, 12, 10))
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
