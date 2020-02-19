45ab18a4b50109d4b2d12178e4aa3b1faff75e18
IL KYOUNG HWANG <sizuha@gngs.co.jp>
2020-02-19 11:00:53 +0900

    ~ support: iOS 8

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SizUtil",
-	platforms: [.iOS(.v10)],
+	platforms: [.iOS(.v8)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
