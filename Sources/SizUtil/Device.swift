dd9524a7d95766ce0d57055a6f776f7add947ffc
Sizuha <sizuha.lily@gmail.com>
2021-11-19 13:17:42 +0900

    iOS device情報追加

--- Sources/SizUtil/Device.swift
+++ Sources/SizUtil/Device.swift
@@ -13,6 +13,7 @@ public enum Model : String {
     iPod3              = "iPod 3",
     iPod4              = "iPod 4",
     iPod5              = "iPod 5",
+    iPod6              = "iPod 6",
 
     //iPad
     iPad2              = "iPad 2",
@@ -113,6 +114,7 @@ public extension UIDevice {
             "iPod3,1"   : .iPod3,
             "iPod4,1"   : .iPod4,
             "iPod5,1"   : .iPod5,
+            "iPod7,1"   : .iPod6,
 
             //iPad
             "iPad2,1"   : .iPad2,
