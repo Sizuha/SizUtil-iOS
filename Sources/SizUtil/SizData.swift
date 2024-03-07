e1feceb28214d84f76ee59696b1bc571b2cf23e0
HWANG ILKYOUNG <sizuha.lily@gmail.com>
2024-03-07 13:36:54 +0900

    add: Index of a substring in a string

--- Sources/SizUtil/SizData.swift
+++ Sources/SizUtil/SizData.swift
@@ -35,6 +35,42 @@ public extension Range {
 
 prefix operator ?=
 
+extension StringProtocol {
+    /*
+     使い方＞
+     let str = "Hello, playground, playground, playground"
+     str.index(of: "play")      // 7
+     str.endIndex(of: "play")   // 11
+     str.indices(of: "play")    // [7, 19, 31]
+     str.ranges(of: "play")     // [{lowerBound 7, upperBound 11}, {lowerBound 19, upperBound 23}, {lowerBound 31, upperBound 35}]
+     */
+    
+    func index<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> Index? {
+        range(of: string, options: options)?.lowerBound
+    }
+    
+    func endIndex<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> Index? {
+        range(of: string, options: options)?.upperBound
+    }
+    
+    func indices<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> [Index] {
+        ranges(of: string, options: options).map(\.lowerBound)
+    }
+    
+    func ranges<S: StringProtocol>(of string: S, options: String.CompareOptions = []) -> [Range<Index>] {
+        var result: [Range<Index>] = []
+        var startIndex = self.startIndex
+        while startIndex < endIndex,
+            let range = self[startIndex...]
+                .range(of: string, options: options) {
+                result.append(range)
+                startIndex = range.lowerBound < range.upperBound ? range.upperBound :
+                    index(range.lowerBound, offsetBy: 1, limitedBy: endIndex) ?? endIndex
+        }
+        return result
+    }
+}
+
 public extension String {
     
     /// スケールとしての時間（TimeInterval）をテキストに変換。例えば「48時間30分」など
